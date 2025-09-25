8b2e0dfae5760356844587458c39b05bd5cdd5ae
Monica Khirani Triastary <monicakhirani273@gmail.com>
2025-09-25 16:22:29 +0700

    Add files via upload

---
+++ foreach.php
@@ -0,0 +1,7 @@
+<?php
+    $mahasiswa = ['Mahasiswa 1', 'Mahasiwa 2', 'Mahasiwa 3', 'Mahasiwa 4', 'Mahasiwa 5'];
+    foreach($mahasiswa as $row){
+        echo $row;
+        echo "<br/>";
+    }
+?>
